797cb077d0606d1597f1cc629f412b4be1002c44
Елена Забудская <zabudskaya@MacBook-Pro-Elena.local>
2019-05-22 23:02:54 +0300

    modified files

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ class Game {
         this.el = {
             main: document.querySelector('.main'),
             form: document.querySelector('.menu__form'),
-            card: document.querySelectorAll('.item'),
-            bug: document.querySelectorAll('.item__face_back')
+            card: document.querySelectorAll('.container__cards .container__card'),
+            bug: document.querySelectorAll('.card__face_back'),
+            container: document.querySelector('.container'),
+            wrap: document.querySelector('.container__wrap'),
+            cards: document.querySelector('.container__cards')
         };
 
         this.isTurn = false;
@@ -17,21 +20,8 @@ class Game {
             this.el.form.reset();
 
             event.preventDefault();
-            this.bug();
-        });
 
-        this.el.card.forEach(item => {
-            item.addEventListener('click', () => {
-                if (this.isTurn) {
-                    this.el.main.dataset.screen = 'menu';
-                    this.isTurn.classList.remove('active');
-                    this.isTurn = false;
-                    this.bugCard.classList.remove('bug');
-                } else {
-                    item.classList.add('active');
-                    this.isTurn = item;
-                }
-            })
+            this.bug();
         });
 
 
@@ -41,18 +31,72 @@ class Game {
              return Math.floor(Math.random() * (max - min + 1)) + min;
 
          }
-        let number;
+         let number;
+         const countSimple = 3;
+         const countMedium = 6;
+         const countComplicated = 10;
+
         if(this.el.main.dataset.screen === 'simple') {
-            number = Math.floor(Math.random() * 3);
+            for(let i = 0; i < countSimple; i++){
+                let card = this.el.wrap.children[0].cloneNode(true);
+                this.el.cards.appendChild(card);
+            }
+
+            this.el.cards.classList.add('simple');
+
+            this.el.bug  = document.querySelectorAll('.card__face_back');
+
+            number = random(1, 3);
             this.bugCard = this.el.bug[number];
-            this.el.bug[number].classList.add('bug');
+            this.bugCard.classList.add('bug');
+
         }else if(this.el.main.dataset.screen === 'medium'){
-            number = random(3, 9);
-            this.el.bug[number].classList.add('bug');
+
+            for(let i = 0; i < countMedium; i++){
+                let card = this.el.wrap.children[0].cloneNode(true);
+                this.el.cards.appendChild(card);
+            }
+
+            this.el.cards.classList.add('medium');
+
+            this.el.bug  = document.querySelectorAll('.card__face_back');
+
+            number = random(1, 6);
+            this.bugCard = this.el.bug[number];
+            this.bugCard.classList.add('bug');
+
         }else if(this.el.main.dataset.screen === 'complicated'){
-            number = random(9, 19);
-            this.el.bug[number].classList.add('bug');
+
+            for(let i = 0; i < countComplicated; i++){
+                let card = this.el.wrap.children[0].cloneNode(true);
+                this.el.cards.appendChild(card);
+            }
+
+            this.el.cards.classList.add('complicated');
+
+            this.el.bug  = document.querySelectorAll('.card__face_back');
+
+            number = random(1, 10);
+            this.bugCard = this.el.bug[number];
+            this.bugCard.classList.add('bug');
         }
+
+        this.el.card = document.querySelectorAll('.container__cards .container__card');
+
+         this.el.card.forEach(item => {
+             item.addEventListener('click', () => {
+                 if (this.isTurn) {
+                     this.el.main.dataset.screen = 'menu';
+                     this.isTurn.classList.remove('active');
+                     this.isTurn = false;
+                     this.bugCard.classList.remove('bug');
+                     this.el.cards.innerHTML = ''
+                 } else {
+                     item.classList.add('active');
+                     this.isTurn = item;
+                 }
+             })
+         });
     };
 }
 
